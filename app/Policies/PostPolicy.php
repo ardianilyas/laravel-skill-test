65e3f6ff5b8b4c998df64638ccfe332056813d41
ardianilyas <ardianilyas@gmail.com>
2025-06-16 21:43:16 +0700

    refactor: update order Post and User model on PostPolicy

--- app/Policies/PostPolicy.php
+++ app/Policies/PostPolicy.php
@@ -15,12 +15,12 @@ class PostPolicy
         //
     }
 
-    public function update(Post $post, User $user)
+    public function update(User $user, Post $post)
     {
         return $user->id === $post->user_id;
     }
 
-    public function delete(Post $post, User $user)
+    public function delete(User $user, Post $post)
     {
         return $user->id === $post->user_id;
     }

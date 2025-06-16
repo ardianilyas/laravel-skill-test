f69fb07955c689aa771d535d857a84566a6a518a
ardianilyas <ardianilyas@gmail.com>
2025-06-16 20:12:57 +0700

    refactor: add user relation to post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,9 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'title',
+        'content',
+        'is_draft',
+        'published_at',
+    ];
+
+    public function user(): BelongsTo {
+        return $this->belongsTo(User::class);
+    }
 }
